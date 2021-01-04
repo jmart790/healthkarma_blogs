7e79b72e8fa19fd8129009a04fc4a31e484f76a2
Joshua Martinez <jmartinez@healthkarma.org>
2021-01-04 14:20:43 -0500

    added empty apiclient file

---
+++ apiClient.js
@@ -0,0 +1,13 @@
+import axios from '~/plugins/axios'
+
+/**
+ *
+ *  @functions are acynchronous so we can await for the response and
+ *             extract the data object which will always contain:
+ *  @status FAILED || ERROR || SUCCESS,
+ *  @message "a message based upon the status from the back end",
+ *  @data an object containing information upon success
+ */
+export default {
+
+}
