c609762cda5efbcbfc1e362b4e37d28128951a5f
Joshua Martinez <jmartinez@healthkarma.org>
2021-01-08 12:52:00 -0500

    updated index blog page to be more responsive

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="blog-page">
-    <h1 class="blog-page__title">Blog Page</h1>
+  <div class="blog-index-page">
+    <h1 class="blog-index-page__title">Blog Page Title</h1>
     <!-- blogs -->
-    <ul class="blog-page__blogs">
-      <li v-for="(blog,id) in testBlogs" :key="id" class="blog-page__blog">
+    <ul class="blog-index-page__blogs">
+      <li v-for="(blog,id) in [...testBlogs, ...testBlogs, ...testBlogs]" :key="id" class="blog-index-page__blog">
         <header class="blog-img-container">
-          <img src="~/assets/images/telehealth_image.png" alt="blog image">
+          <img :src="blog.image" :alt="`${blog.title} image`" loading="lazy">
         </header>
         <div class="blog-content">
           <h2 class="blog-content__title">{{blog.title}}</h2>
@@ -14,9 +14,10 @@
             variant="terciary"
             class="px-0"
             @click="$router.push(`/${blog.url}`)"
+            icon="icon-navigation-linear-chevron-right"
+            flipContent
           >
-            Read More
-            <h-icon icon="icon-navigation-linear-chevron-right" size="small" class="d-inline"/>
+            <span class="font-600">Read More</span>
           </h-button>
         </div>
       </li>
@@ -26,20 +27,24 @@
 </template>
 
 <script>
-import Stack from "../plugins/entry";
-
 export default {
   async asyncData() {
     return {
       testBlogs: [
         { title: "Eddy's favorite cupcake recipes to stay in shape",
+          image: "https://www.biggerbolderbaking.com/wp-content/uploads/2017/09/1C5A0996.jpg",
+          content: "Lorem ipsum dolore sot amet, sit dolor sit, lorem ipsum dolore sit amet, sit sit dolore lorem ipsum",
           url: 'eddies-cupcake-recipes'
         },
         { title: "Josh's top plants for small spaces",
+          image: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/various-beautiful-green-plants-in-pots-on-white-royalty-free-image-931824676-1565950537.jpg?crop=0.825xw:0.620xh;0.0785xw,0.132xh&resize=1200:*",
+          content: "Lorem ipsum dolore sot amet, sit dolor sit, lorem ipsum dolore sit amet, sit sit dolore lorem ipsum",
           url: 'plants-by-josh'
         },
         {
           title: "Save more with Carl's recomended RX meds",
+          image: "https://s.abcnews.com/images/Health/GTY_pill_bottle_kab_140618_16x9_992.jpg",
+          content: "Lorem ipsum dolore sot amet, sit dolor sit, lorem ipsum dolore sit amet, sit sit dolore lorem ipsum",
           url: 'carlys-fav-meds'
         }
         ]
@@ -49,25 +54,45 @@ export default {
 </script>
 
 <style lang="scss">
-.blog-page {
+.blog-index-page {
   padding: 12px 20px;
+  @media screen and (min-width: $laptop) { 
+    padding: 32px 36px;
+  }
   &__title {
     font-size: 21px;
     font-weight: bold;
     line-height: 1.52;
     text-align: center;
     color: $black-light;
+    @media screen and (min-width: $laptop) { 
+      margin-bottom: 32px;
+      font-size: 28px;
+    }
   }
   &__blogs {
-    list-style: none;
+    display: grid;
+    justify-content: center;
+    grid-template-columns: repeat(auto-fit, 335px);
+    grid-gap: $spacing-s;
     margin: unset;
     padding: unset;
+    list-style: none;
+    @media screen and (min-width: $laptop) { 
+      grid-template-columns: repeat(auto-fit, 308px);
+      grid-row-gap: 24px;
+      grid-column-gap: 20px;
+    }
   }
   &__blog {
-    width: 335px;
-    height: 323px;
-    margin-bottom: $spacing-s;
-    box-shadow: 0 6px 10px 0 #dfecff;
+    min-height: 323px;
+    max-width: 335px;
+    width: 100%;
+    background-color: $white;
+    box-shadow: 0 6px 10px 0 $blue-light;
+    @media screen and (min-width: $laptop) { 
+      max-width: 308px;
+    }
     .blog-img-container {
       height: 130px;
       overflow: hidden;
@@ -84,11 +109,20 @@ export default {
         font-weight: bold;
         line-height: 1.5;
         color: $black-light;
+        @media screen and (min-width: $laptop) { 
+          margin-bottom: $spacing_s;
+          font-size: 18px;
+        }
       }
       &__snippet {
         font-size: 14px;
         line-height: 1.43;
-        color: #272f37;
+        color: $black-light;
+        @media screen and (min-width: $laptop) { 
+          font-size: 16px;
+          line-height: 24px;
+          color: $gray-medium-light;
+        }
       }
       &__link {
         color: $secondary1;
