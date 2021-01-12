83a75d1c095f664c7cecdd4d0d0485892d0047cc
Joshua Martinez <jmartinez@healthkarma.org>
2021-01-12 10:16:48 -0500

    dynamically showing any available social media links

--- components/blog/BlogArticle.vue
+++ components/blog/BlogArticle.vue
@@ -1,35 +1,32 @@
 <template>
   <div class="blog-article">
     <h1 class="blog-article__title">{{article.title}}</h1>
-    <p class="blog-article__date">{{article.date}}</p>
-    <p class="blog-article__blurb">{{article.blurb}}</p>
+    <p class="blog-article__date">{{article.date_of_published_blog}}</p>
+    <div class="blog-article__blurb" v-html="richText"></div>
     <div class="blog-article__links-container">
       <h-button 
         variant="terciary" 
         class="blog-article__link"
       ><b>Share</b>
       </h-button>
-      <h-button 
-        variant="link" 
-        :isCircle="true"
-        class="blog-article__link"
-      >
-        <h-icon icon="icon-brand-facebook" size="large"/>
-      </h-button>
-      <h-button 
-        variant="link" 
-        :isCircle="true"
-        class="blog-article__link"
-      >
-        <h-icon icon="icon-brand-instagram" size="large"/>
-      </h-button>
-      <h-button 
-        variant="link" 
-        :isCircle="true"
-        class="blog-article__link"
-      >
-        <h-icon icon="icon-brand-twitter" size="large"/>
-      </h-button>
+      <template v-if="article.social.length">
+        <a 
+          v-for="(item,id) in article.social[0].social_share" 
+          :key="id+'-social'"
+          target="_blank" 
+          :href="item.link.href"
+        >
+          <h-button 
+            variant="link" 
+            :isCircle="true"
+            class="blog-article__link"
+          >
+            <h-icon 
+              :icon="`icon-brand-${item.link.title.toLowerCase()}`" 
+              size="large"/>
+          </h-button>
+        </a>
+      </template>
     </div>
   </div>
 </template>
@@ -40,8 +37,14 @@ export default {
     article: {
       type: Object,
       required: true
+    },
+  },
+  computed: {
+    richText() {
+      if (this.article.blog_copy) return this.article.blog_copy.rich_text_editor || '';
+      return ''
     }
-  }
+  },
 }
 </script>
 
@@ -60,12 +63,16 @@ export default {
     }
   }
   &__date {
+    font-size: 14px;
+    line-height: 1.43;
+    color: $black-light;
     @media screen and (min-width: $laptop) { 
       margin-bottom: 32px;
     }
   }
-  &__date, &__blurb {
+  &__blurb {
     max-width: 855px;
+    margin-bottom: 32px;
     font-size: 14px;
     line-height: 1.43;
     color: $black-light;
@@ -73,13 +80,16 @@ export default {
       font-size: 16px;
       line-height: 24px;
     }
+    ul, ol {
+      margin-left: 32px;
+    }
   }
   &__links-container {
     display: flex;
     align-items: center;
   }
   &__link {
-    padding: 0;
+    padding: 8px;
     margin-right: $spacing_m;
     text-decoration: none;
   }
