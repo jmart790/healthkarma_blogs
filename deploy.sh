b340a5f5bb64740a172bb3644ca2b7b9e3bce538
Asdruval De Leon <me@asdruval.com>
2021-02-02 17:36:00 -0500

    update deploy script and fix typo on ecosystem file.

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,52 @@
-#!/bin/bash
+#!/bin/sh
+export PATH=node_modules/.bin/:$PATH
+export DISABLE_NOTIFIER=true
 
-deployed_moment=`date +%F-%T`
-if git status | grep 'nothing to commit, working tree clean'; then
+START=$(date +%s)
 
-        PULL="$(git pull origin master)"
+rev=$(git rev-parse --short HEAD)
+protorev='1';
+git rev-parse --short HEAD > revision.json
+cp -f revision.json public/revision.json
+oldrev=$(curl https://blog.healthkarma.org/version.txt)
 
-        if [$PULL | grep '\<.*.vue\>'] || [$PULL | grep '\<.*.js\>'] || [$PULL | grep '\<.*.scss\>'] || [$PULL | grep '\<.*.css\>']; then
-                echo "Node"
-                if $1 = 'dev'; then
-                        npm run dev
-                else
-                        npm run build
-                        pm2 start
-                fi
-        fi
+echo "Clearing out old revisions..."
+find -P public/js/ -name '*.js' -type f -ctime +14 -delete
+find -P public/js/ -name '*.css' -type f -ctime +14 -delete
+find -P public/js/ -name '*.json' -type f -ctime +14 -delete
+find -P public/static/js/ -name '*.js' -type f -ctime +14 -delete
+find -P public/static/css/ -name '*.css' -type f -ctime +14 -delete
 
-else
-        echo "Dirty branch. Check first"
-        exit
-fi
+echo "Building out revision:"
+echo $rev
+
+echo "Old revision:"
+echo $oldrev
+
+echo "Diff URL: https://gitlab.dream-foundation.com/healthkarma/blogkarma_fe/-/compare/$oldrev...$rev"
+
+# build out angular frontend
+echo "Building Nuxt Blog..."
+npm run build
+pm2 reload all
+
+#if git status | grep 'nothing to commit, working tree clean'; then
+#
+#        PULL="$(git pull origin master)"
+#
+#
+#
+#        if [$PULL | grep '\<.*.vue\>'] || [$PULL | grep '\<.*.js\>'] || [$PULL | grep '\<.*.scss\>'] || [$PULL | grep '\<.*.css\>']; then
+#                echo "Node"
+#                if $1 = 'dev'; then
+#                        npm run dev
+#                else
+#                        npm run build
+#                        pm2 start
+#                fi
+#        fi
+#
+#else
+#        echo "Dirty branch. Check first"
+#        exit
+#fi
