05d1714c2b6ec50c19107d98726c73b33cdd1564
Asdruval De Leon <me@asdruval.com>
2021-02-18 17:36:32 -0500

    update git command

--- deploy.sh
+++ deploy.sh
@@ -4,26 +4,8 @@ export DISABLE_NOTIFIER=true
 
 START=$(date +%s)
 
-rev=$(git rev-parse --short HEAD)
-protorev='1';
-git rev-parse --short HEAD > revision.json
-cp -f revision.json public/revision.json
-oldrev=$(curl https://blog.healthkarma.org/version.txt)
-
-echo "Clearing out old revisions..."
-find -P public/js/ -name '*.js' -type f -ctime +14 -delete
-find -P public/js/ -name '*.css' -type f -ctime +14 -delete
-find -P public/js/ -name '*.json' -type f -ctime +14 -delete
-find -P public/static/js/ -name '*.js' -type f -ctime +14 -delete
-find -P public/static/css/ -name '*.css' -type f -ctime +14 -delete
-
-echo "Building out revision:"
-echo $rev
-
-echo "Old revision:"
-echo $oldrev
-
-echo "Diff URL: https://gitlab.dream-foundation.com/healthkarma/blogkarma_fe/-/compare/$oldrev...$rev"
+echo "Pulling from gitlab"
+git pull origin master
 
 # build out angular frontend
 echo "Building Nuxt Blog..."
