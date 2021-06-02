ca5febd45e95e2feeb0596c95435a8c2309447e4
mariorosenthal <mrosenthal@healthkarma.org>
2021-06-02 18:10:10 -0400

    adding nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,6 +16,19 @@ export default {
       })
     }
   },
+  head: {
+    title: 'my website title',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'my website description'
+      }
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+  },
   // head: {
   //   htmlAttrs: {lang: 'en'},
   //   title: 'For the Health of it',
